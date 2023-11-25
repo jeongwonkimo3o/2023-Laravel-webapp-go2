6853e6837ad05367eefc3c60cf21344d3b803efc
gardenkim <oyasumiee2@gmail.com>
2023-11-25 21:18:59 +0900

    Design: Add Word Page

---
+++ resources/js/Pages/Word.vue
@@ -0,0 +1,34 @@
+<template>
+  <HeaderVue />
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 ">
+    <header class="flex justify-between items-end">
+      <h2 class="text-xl font-medium text-gray-900 sm:text-2xl">
+        김가든님의 단어장 리스트입니다.
+      </h2>
+    </header>
+    <WordTitleCardVue class="mt-10" />
+    <WordTitleCardVue class="mt-6" />
+    <WordTitleCardVue class="mt-6" />
+    <WordTitleCardVue class="mt-6" />
+  </div>
+  <FooterVue />
+</template>
+
+<script>
+import HeaderVue from '@/Components/Header.vue'
+import WordTitleCardVue from '@/Components/WordTitleCard.vue'
+import FooterVue from '@/Components/Footer.vue'
+
+
+export default {
+  components: {
+    HeaderVue,
+    WordTitleCardVue,
+    FooterVue
+  }
+}
+</script>
+
+<style>
+
+</style>
