170cf3bb2031e972f0536b39f16a21133349b5a6
gardenkim <oyasumiee2@gmail.com>
2023-12-12 16:16:59 +0900

    Feat: Implement Words Table Model

---
+++ app/Models/Word.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Word extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'title_id',      // 단어장 ID
+      'word',          // 단어
+      'mean',          // 의미
+      'pos',           // 품사
+      'pronunciation', // 발음
+      'synonym',       // 동의어
+      'antonym',       // 반의어
+      'explain'        // 설명
+     ];
+
+    
+}
