7b257151a9215e38dbd4ca2369ee2e81f19dfeaa
gardenkim <oyasumiee2@gmail.com>
2023-12-13 23:54:38 +0900

    Chore: Routing Theorem

--- routes/web.php
+++ routes/web.php
@@ -35,64 +35,67 @@ Route::get('/', function () {
 
 // ^ 업로드 관련
 
-// 업로드 페이지 렌더링
-Route::get('/upload', function() {
-    return Inertia::render('Upload');
+Route::middleware(['auth'])->group(function () {
+    // 업로드 페이지 렌더링
+    Route::get('/upload', function() {
+        return Inertia::render('Upload');
+    });
+
+    // 단어 파일(csv) 업로드
+    Route::post('/upload', [UploadController::class, 'upload']);
+
+    // 방금 업로드한 단어장 이름 조회
+    Route::get('/upload-title', [WordTitlesController::class, 'getUploadWordTitles']);
+
+    // 단어장 언어 설정
+    Route::post('/setting-lang/{id}', [UploadController::class, 'updateLanguage']);
+
+    // 단어장 관련 라우트 끝
+
+    // 단어장 렌더링
+    Route::get('/word', function () {
+        return Inertia::render("Word");
+    });
+    
+    // 단어장 목록 조회
+    Route::get('/titles', [WordTitlesController::class, 'getAllWordTitles']);
+
+    // 단어장 언어별 조회
+    Route::get('/titles/{lang}', [WordTitlesController::class, 'getWordTitlesByLang']);
+
+    // 단어장에 속한 단어들 조회
+    Route::get('/worddetail/{id}', [WordsController::class, 'getWords']);
+
+    // 단어장 삭제
+    Route::delete('/delete-title/{id}', [WordTitlesController::class, 'deleteWordTitle']);
+
+    // 단어 추가
+    Route::post('/add-word', [WordsController::class, 'addWord']);
+
+    // 단어 삭제
+    Route::delete('/delete-word/{id}', [WordsController::class, 'deleteWord']);
+
+    // 단어 csv 파일로 내보내기
+    Route::get('/export-wordtitle/{wordTitleId}', [CSVExportController::class, 'export']);
+
+    // 단어장 스토어 관련 라우트 끝
+
+    // 단어장 스토어 렌더링
+    Route::get('/store', function () {
+        return Inertia::render('Store');
+    });
+
+    // 스토어에 저장된 단어장 목록 조회
+    Route::get('/stored-word-titles', [StoreController::class, 'getStoredWordTitles']);
+
+    // word_titles 테이블 store 변경
+    Route::post('/word-titles/{id}/store', [StoreController::class, 'toggleStoreWordTitle']);
+
+    // 스토어에 저장된 단어장 다운로드
+    Route::post('/download-word-title/{wordTitleId}', [StoreController::class, 'downloadWordTitle']);
 });
 
-// 단어 파일(csv) 업로드
-Route::post('/upload', [UploadController::class, 'upload']);
 
-// 방금 업로드한 단어장 이름 조회
-Route::get('/upload-title', [WordTitlesController::class, 'getUploadWordTitles']);
-
-// 단어장 언어 설정
-Route::post('/setting-lang/{id}', [UploadController::class, 'updateLanguage']);
-
-// ^ 단어장 관련
-
-// 단어장 렌더링
-Route::get('/word', function () {
-    return Inertia::render("Word");
-});
-// 단어장 목록 조회
-Route::get('/titles', [WordTitlesController::class, 'getAllWordTitles']);
-
-// 단어장 언어별 조회
-Route::get('/titles/{lang}', [WordTitlesController::class, 'getWordTitlesByLang']);
-
-// 단어장에 속한 단어들 조회
-Route::get('/worddetail/{id}', [WordsController::class, 'getWords']);
-
-// 단어장 삭제
-Route::delete('/delete-title/{id}', [WordTitlesController::class, 'deleteWordTitle']);
-
-// 단어 추가
-Route::post('/add-word', [WordsController::class, 'addWord']);
-
-// 단어 삭제
-Route::delete('/delete-word/{id}', [WordsController::class, 'deleteWord']);
-
-// 단어 csv 파일로 내보내기
-Route::get('/export-wordtitle/{wordTitleId}', [CSVExportController::class, 'export']);
-
-
-// ^ 단어장 스토어 관련
-
-// 단어장 스토어 렌더링
-Route::get('/store', function () {
-    return Inertia::render('Store');
-});
-
-// 스토어에 저장된 단어장 목록 조회
-Route::get('/stored-word-titles', [StoreController::class, 'getStoredWordTitles']);
-
-
-// word_titles 테이블 store 변경
-Route::post('/word-titles/{id}/store', [StoreController::class, 'toggleStoreWordTitle']);
-
-// 스토어에 저장된 단어장 다운로드
-Route::post('/download-word-title/{wordTitleId}', [StoreController::class, 'downloadWordTitle']);
 
 Route::middleware([
     'auth:sanctum',
