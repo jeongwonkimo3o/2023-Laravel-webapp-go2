dfae24bdf7bf8251f1229d111b3ef1c78fbecd02
gardenkim <oyasumiee2@gmail.com>
2023-12-13 23:55:40 +0900

    Design: Change Tailwind style properties

--- resources/js/Components/StoreCard.vue
+++ resources/js/Components/StoreCard.vue
@@ -29,28 +29,6 @@
     </div>
   </div>
 
-  <div class="flex justify-end">
-    <strong
-      class="-mb-[2px] -me-[2px] inline-flex items-center gap-1 rounded-ee-xl rounded-ss-xl bg-pink-600 px-3 py-1.5 text-white"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-        />
-      </svg>
-
-      <span class="text-[10px] font-medium sm:text-xs">이미 있어요</span>
-    </strong>
-  </div>
 </article>
 </template>
 
